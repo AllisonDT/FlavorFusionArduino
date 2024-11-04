684fe263843cec45cebd6084b396af29b08e6812
Pabon02 <samuelpabon02@gmail.com>
2024-11-04 03:43:51 -0500

    Added spice menus
    LCD now responds to button feedback and menu navigation has been smoothed. Speed is improved by only scanning for either BLE or LCD inputs at a time. Stopped buttons from registering multiple times in a row. Spice selections from the LCD now call processReceivedIngredients() and send spice amounts to motors. Still need to fix some UI bugs on spice amount menu (box size, arrows disappearing, numbers don't update) and spice selection menu (list goes off screen, users can't return to spice selection after an order finishes).

--- FlavorFusionArduino.ino
+++ FlavorFusionArduino.ino
@@ -14,6 +14,8 @@ String dataBuffer = "";        // Global buffer to accumulate incoming data
 bool isSusanRotated = 0;
 bool isRailForward = 0;
 int numSpicesOrdered;         // Number of spices in current job
+bool isOrderInput = 0;
+bool isBLEOrder = 1; // Bluetooth vs manual order
 
 // Variables to track previous states
 bool prevIsOrderMixed = false;
@@ -60,6 +62,17 @@ U8GLIB_ST7920_128X64_1X u8g(LCD_SCK, LCD_MOSI, LCD_CS);
 bool menu_redraw_required = 0;
 int encoderValue = 0;  // Count of encoder turns and cursor position
 bool lastStateA = HIGH; // Previous state of encoder pin A
+bool currentMenu = 0; // 0: spice menu,   1: amount menu
+uint8_t amountColumn = 0; // 0: spice integer,   1: spice fraction,   2: spice unit,   3: confirm
+uint8_t maxBound = 1; // max index for a menu
+// Buffer variable for spice amount menu:
+int tempSpiceNumber = 0; 
+int tempSpiceInt = 0;
+float tempSpiceFraction = 0.0;
+uint8_t tempSpiceDivider = 1; // unit conversion
+String tempSpiceString = "";
+
+const char *menu_strings[totalSpices+1] = { "Confirm order", "Spice 1", "Spice 2", "Spice 3", "Spice 4", "Spice 5", "Spice 6", "Spice 7", "Spice 8", "Spice 9", "Spice 10"};
 
 
 void setup() {
@@ -90,12 +103,7 @@ void setup() {
   Serial1.begin(9600);
 
   u8g.setColorIndex(1);      // Set color to white  
-
-  // Setup picture loop
-  u8g.firstPage();
-  do {
-    drawSetup();
-  } while( u8g.nextPage());
+  menu_redraw_required = 1;     // force initial redraw
 }
 
 void loop() {
@@ -117,10 +125,44 @@ void loop() {
     }
   }
 
-  // Check if data is available from LCD
-  /*
-  updateLCD();
-  */
+  //Check for user click input
+  if(!digitalRead(LCD_push)){
+    while(!digitalRead(LCD_push)){
+      delay(100); // add delay to prevent double counting button press
+    }
+    isBLEOrder = 0;
+  }
+
+  // Setup picture loop for BLE order
+  if(isBLEOrder){
+    u8g.firstPage();
+    do {
+      drawSetup();
+    } while( u8g.nextPage());
+  }
+
+  //Spice menu loop for manual order
+  while(!isOrderInput && !isBLEOrder){
+
+    // Check if data is available from LCD
+    updateLCD();
+  
+    // update scroll bar
+    if (menu_redraw_required) {
+      u8g.firstPage();
+      do  {
+        // check for current menu to draw
+        if(!currentMenu){
+          drawSpiceMenu();
+        }else if(currentMenu){
+          drawAmountMenu();
+        }
+          
+      } while( u8g.nextPage() );
+      menu_redraw_required = 0;
+    }
+  }
+  
 
   // Process spice data if the order has been received and not mixed
   if (!isOrderMixed) {
@@ -385,7 +427,15 @@ void moveAuger(int j) {
   do {
     u8g.drawStr(0, 0, "Dispensed ");
     int w = u8g.getStrPixelWidth("Dispensed ");
-    u8g.drawStr(w, 0, spiceArray[j][1].c_str()); // convert String object to const char*
+
+    // Find decimal point index
+    int decimalIndex = spiceArray[j][1].indexOf('.');
+    // Truncate string after 5 decimal places
+    String truncatedString = spiceArray[j][1].substring(0, decimalIndex+6);
+    // convert String object to const char*
+    const char *truncatedAmount = truncatedString.c_str();
+
+    u8g.drawStr(w, 0, truncatedAmount); 
     int h = u8g.getFontAscent() - u8g.getFontDescent();
     u8g.drawStr(0, h+1, "oz of spice");
   } while( u8g.nextPage());
@@ -408,18 +458,33 @@ void calibrate() {
   delay(2000); // Remove this line!!!
 }
 
+
 void drawSetup() {
   uint8_t h;
+  u8g_uint_t w, d;
   
   u8g.setFont(u8g_font_5x8);
   u8g.setFontRefHeightText(); // calculate font height based on text only
   u8g.setFontPosTop(); // reference lines from top left instead of bottom left
 
   h = u8g.getFontAscent()-u8g.getFontDescent(); // font height
+  w = u8g.getWidth();
 
-  u8g.drawStr( 0, 0, "HM-10 BLE Module Test");  
-  u8g.drawStr( 0, h+1, "Waiting for data...");
+  d = (w - u8g.getStrPixelWidth("Waiting for order from"))/2;
+  u8g.drawStr( d, 0, "Waiting for order from");  
+
+  d = (w - u8g.getStrPixelWidth("Flavor Fusion app..."))/2;
+  u8g.drawStr( d, h+1, "Flavor Fusion app...");
+
+  d = (w - u8g.getStrPixelWidth("or"))/2;
+  u8g.drawStr( d, 3*h+1, "or"); 
+
+  d = (w - u8g.getStrPixelWidth("Click once to"))/2;
+  u8g.drawStr( d, 5*h+1, "Click once to");
+  d = (w - u8g.getStrPixelWidth("order manually."))/2;
+  u8g.drawStr( d, 6*h+1, "order manually.");
 }
+
 
 void updateLCD() {
   // Read the current state of the encoder pins
@@ -430,16 +495,111 @@ void updateLCD() {
   if (lastStateA == HIGH && currentStateA == LOW) {
     // change cursor position based on direction of B
     encoderValue += (currentStateB == HIGH) ? 1 : -1;
+    delay(25); // slight delay for smoothness
     menu_redraw_required = 1;
 
     // Keep cursor in menu bounds
-    if(encoderValue >= totalSpices){
-      encoderValue = 0;
-      delay(50); // slight delay to avoid jitter 
+    if(!currentMenu){
+      // Spice menu bounds
+      maxBound = totalSpices; // 10 spices plus confirm button
+    }else{
+      // Amount menu bounds
+      if(amountColumn == 0){
+        // Spice amount (integer)
+        maxBound = 9; // limit selection to 9
+
+      }else if(amountColumn == 1){
+        // Spice amount (fraction)
+        maxBound = 7; // limit selection to 7/8
+
+      }else if(amountColumn == 2){
+        // Spice unit
+        maxBound = 2; // tsp, Tbsp, oz
+      }
     }
-    else if(encoderValue < 0){
-      encoderValue = totalSpices-1;
-      delay(50); // slight delay to avoid jitter 
+
+    if(encoderValue > maxBound){
+        encoderValue = 0;
+        delay(50); // slight delay to avoid jitter 
+      }
+      else if(encoderValue < 0){
+        encoderValue = maxBound-1;
+        delay(50); // slight delay to avoid jitter 
+      }
+
+  }
+
+  // Check for encoder button push
+  if(!digitalRead(LCD_push)){
+    while(!digitalRead(LCD_push)){
+      delay(100); // add delay to prevent double counting button press
+    }
+    menu_redraw_required = 1;
+    
+    // Change menu or column depending on curent screen
+    if(!currentMenu){
+      // Spice menu:
+      if(encoderValue == 0){
+        processReceivedIngredients(tempSpiceString); // process data into spiceArray
+        isOrderInput = 1; // confirm order
+        tempSpiceString = ""; // clear buffer
+      }else{
+      tempSpiceNumber = encoderValue; // save selected spice into buffer
+      currentMenu = !currentMenu; // advance from spice menu to amount menu
+      encoderValue = 0; // reset cursor value
+    }
+    // Amount menu:
+    }else if(amountColumn != 3){
+      // save selected amount into buffer
+      if(amountColumn == 0){
+        tempSpiceInt = encoderValue; // whole number
+      }else if(amountColumn == 1){
+        tempSpiceFraction = encoderValue*0.125; // fractions (1/8 increment)
+      }else{
+        if(encoderValue == 0){
+          tempSpiceDivider = 6; // convert tsp to oz
+        }else if(encoderValue == 1){
+          tempSpiceDivider = 2; // convert Tbsp to oz
+        }else{
+          tempSpiceDivider = 1; // convert oz to oz
+        }
+      }
+      amountColumn++; // advance column number in amount menu
+      encoderValue = 0; // reset cursor
+    }else{
+      tempSpiceString += String(tempSpiceNumber) + ":"; // store spice number and amount 
+      tempSpiceString += String((tempSpiceInt + tempSpiceFraction) / tempSpiceDivider) + ";";
+      tempSpiceNumber = 0; // clear buffer
+      tempSpiceInt = 0;
+      tempSpiceFraction = 0.0;
+      tempSpiceDivider = 1;
+      amountColumn = 0; // reset column
+      currentMenu = !currentMenu; // if amount confirmed, return to spice menu
+    }
+    
+  }
+
+  // Check for stop button push
+  if(!digitalRead(LCD_stop)){
+    while(!digitalRead(LCD_stop)){
+      delay(100); // add delay to prevent double counting button press
+    }
+    menu_redraw_required = 1;
+
+    // Change menu or column depending on curent screen
+    if(!currentMenu){
+      // Spice menu:
+      isBLEOrder = 1; // return to start menu
+      encoderValue = 0; // reset cursor
+    }
+    // Amount menu:
+    else if(amountColumn != 0){
+      amountColumn--; // retract column number in amount menu
+      encoderValue = 0; // reset cursor
+      // delete spice amount from buffer
+    }else{
+      currentMenu = !currentMenu; // return to spice menu
+      encoderValue = 0; // reset cursor
     }
   }
 
@@ -458,10 +618,165 @@ void drawSummary() {
 
     for(int j = 0; j < numSpicesOrdered; j++) {
       u8g.drawStr(0, (j+2)*h+3, spiceArray[j][0].c_str()); // convert String object to const char*
-      u8g.drawStr(64, (j+2)*h+3, spiceArray[j][1].c_str());
+
+      // Find decimal point index
+      int decimalIndex = spiceArray[j][1].indexOf('.');
+      // Truncate string after 5 decimal places
+      String truncatedString = spiceArray[j][1].substring(0, decimalIndex+6);
+      // convert String object to const char*
+      const char *truncatedAmount = truncatedString.c_str();
+
+      u8g.drawStr(64, (j+2)*h+3, truncatedAmount);
     }
 
     // eventually add a cursor to fit all spices on screen and allow user navigation
 
   } while( u8g.nextPage());
+}
+
+void drawSpiceMenu() {
+  uint8_t i, h;
+  u8g_uint_t w, d;
+
+  u8g.setFont(u8g_font_5x8);
+  u8g.setFontRefHeightText(); // line height based on text chars
+  u8g.setFontPosTop(); // line position references top left of text
+  
+  h = u8g.getFontAscent()-u8g.getFontDescent(); // text height
+  w = u8g.getWidth(); // screen width
+  for( i = 0; i < totalSpices; i++ ) {
+    d = (w-u8g.getStrWidth(menu_strings[i]))/2;
+    u8g.setDefaultForegroundColor();
+    if ( i == encoderValue ) {
+      u8g.drawBox(0, i*h+1, w, h);
+      u8g.setDefaultBackgroundColor();
+    }
+    u8g.drawStr(d, i*h, menu_strings[i]);
+
+    // ADD LIST CASCADE 
+
+  }
+}
+
+void drawAmountMenu(){
+  uint8_t i, h;
+  u8g_uint_t w, d, l, s, m;
+
+  u8g.setFont(u8g_font_5x8);
+  u8g.setFontRefHeightText(); // line height based on text chars
+  u8g.setFontPosTop(); // line position references top left of text
+
+  h = u8g.getFontAscent()-u8g.getFontDescent(); // text height
+  w = u8g.getWidth(); // screen width
+
+  // Spice name at top of menu
+  char buffer[10];
+  itoa(tempSpiceNumber, buffer, 10);
+  const char *spiceNumber = buffer;
+  d = (w - (u8g.getStrWidth("Spice ") + u8g.getStrWidth(spiceNumber)))/2;
+  u8g.drawStr(d, 0, "Spice ");
+  u8g.drawStr(d+u8g.getStrWidth("Spice "), 0, spiceNumber);
+
+  // define labels
+  const char *labels[4] = {"Whole", "Fraction", "Unit", "Done"};
+  s = (w - u8g.getStrWidth("WholeFractionUnitDone")) / 3; // space between labels
+  l = 0; //length of labels so far
+  
+  // Loop thru columns
+  for(i = 0; i < 4; i++){
+
+    u8g.drawStr(l, 2*h, labels[i]); // labels
+
+    m = l + (u8g.getStrWidth(labels[i]))/2; // triangle midpoint
+
+    // check mark
+    uint8_t checkWidth = 24;
+    uint8_t checkHeight = 16;
+    const uint8_t checkmark_bits[] = {
+    0x00, 0x00, 0x00,  // Row 0
+    0x00, 0x00, 0x70,  // Row 1
+    0x00, 0x00, 0xE0,  // Row 2
+    0x00, 0x01, 0xC0,  // Row 3
+    0x00, 0x03, 0x80,  // Row 4
+    0x00, 0x07, 0x00,  // Row 5
+    0x00, 0x0E, 0x00,  // Row 6
+    0x00, 0x1C, 0x00,  // Row 7
+    0x00, 0x38, 0x00,  // Row 8
+    0x60, 0x70, 0x00,  // Row 9
+    0x70, 0xE0, 0x00,  // Row 10
+    0x31, 0xC0, 0x00,  // Row 11
+    0x3B, 0x80, 0x00,  // Row 12
+    0x1F, 0x00, 0x00,  // Row 13
+    0x0E, 0x00, 0x00,  // Row 14
+    0x04, 0x00, 0x00   // Row 15
+    };
+    
+    // check for cursor box
+    if (i == amountColumn) {
+      if(i != 3){
+        u8g.drawBox(m-5, 3*h+2, 11, 18);
+        u8g.setDefaultBackgroundColor();
+        u8g.drawTriangle(m-4,4*h, m+4,4*h, m,3*h+2); // top triangle
+        u8g.drawTriangle(m-4,5*h+1, m,6*h-2, m+4,5*h+1); // bottom triangle
+        // Selection values
+        if(i==0){
+          itoa(tempSpiceInt, buffer, 10);
+          u8g.drawStr(m-2, 4*h, buffer); // whole number
+        }else if(i==1){
+          u8g.setColorIndex(1);
+          u8g.drawBox(m-13, 3*h+2, 27, 18); //expand the box
+          u8g.setDefaultBackgroundColor();
+          dtostrf(tempSpiceFraction, 5, 3, buffer);
+          u8g.drawStr(m-12, 4*h, buffer); // fraction
+        }else if(i==2){
+          if(tempSpiceDivider==1){
+            u8g.drawStr(m-u8g.getStrWidth("oz")/2, 4*h, "oz"); // unit: oz
+          }else if(tempSpiceDivider==2){
+            u8g.drawStr(m-u8g.getStrWidth("Tbsp")/2, 4*h, "Tbsp"); // unit: Tbsp
+          }else if(tempSpiceDivider==6){
+            u8g.drawStr(m-u8g.getStrWidth("tsp")/2, 4*h, "tsp"); // unit: tsp
+          }
+        }
+      }else{
+        u8g.drawBox(m-11, 3*h+2, 24, 18);
+        u8g.setDefaultBackgroundColor();
+        u8g.drawBitmap(m-10, 3*h+3, checkWidth/8, checkHeight, checkmark_bits); //draw checkmark
+      }
+      u8g.setColorIndex(1);
+    }else{
+      // else print normally
+
+      // Triangles
+      if(i != 3){
+      u8g.drawTriangle(m-4,4*h, m+4,4*h, m,3*h+2); // top triangle
+      u8g.drawTriangle(m-4,5*h+1, m,6*h-2, m+4,5*h+1); // bottom triangle
+      }
+
+      // Selection values
+      if(i==0){
+        itoa(tempSpiceInt, buffer, 10);
+        u8g.drawStr(m-2, 4*h, buffer); // whole number
+      }else if(i==1){
+        dtostrf(tempSpiceFraction, 5, 3, buffer);
+        u8g.drawStr(m-12, 4*h, buffer); // fraction
+      }else if(i==2){
+        if(tempSpiceDivider==1){
+          u8g.drawStr(m-u8g.getStrWidth("oz")/2, 4*h, "oz"); // unit: oz
+        }else if(tempSpiceDivider==2){
+          u8g.drawStr(m-u8g.getStrWidth("Tbsp")/2, 4*h, "Tbsp"); // unit: Tbsp
+        }else if(tempSpiceDivider==6){
+          u8g.drawStr(m-u8g.getStrWidth("tsp")/2, 4*h, "tsp"); // unit: tsp
+        }
+      }
+
+      // Check mark
+      if(amountColumn != 3 && i == 3){
+      u8g.drawBitmap(m-10, 3*h+3, checkWidth/8, checkHeight, checkmark_bits);
+    }
+      
+    }
+
+    l += u8g.getStrWidth(labels[i]) + s;
+  }
+
 }
