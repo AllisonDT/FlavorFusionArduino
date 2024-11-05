3044aa88621223042e7258d2dc10e8341a1abfa3
Pabon02 <samuelpabon02@gmail.com>
2024-11-05 18:56:01 -0500

    LCD almost done
    Lots of bug fixing, restructured LCD menus and input sensing, added timeout and button press to return to setup from summary, added random spicy sayings, changed spice amounts to show fractions. Still need to add screen scrolling to spice menu and summary. Also need to try fixing jittery cursor.

--- FlavorFusionArduino.ino
+++ FlavorFusionArduino.ino
@@ -11,11 +11,10 @@ String incomingString = "";
 bool isOrderMixed = true;     // Track whether the whole order is mixed
 bool isTrayEmpty = true;       // Track whether the tray is empty
 String dataBuffer = "";        // Global buffer to accumulate incoming data
-bool isSusanRotated = 0;
-bool isRailForward = 0;
+bool isSusanRotated = 0; // Note: can prolly remove this since we'll calibrate every time
+bool isRailForward = 0; // Note: can prolly remove this
 int numSpicesOrdered;         // Number of spices in current job
-bool isOrderInput = 0;
-bool isBLEOrder = 1; // Bluetooth vs manual order
+bool isOrderInput = 0; // Note: can prolly remove this
 
 // Variables to track previous states
 bool prevIsOrderMixed = false;
@@ -60,19 +59,36 @@ U8GLIB_ST7920_128X64_1X u8g(LCD_SCK, LCD_MOSI, LCD_CS);
 
 // LCD variables
 bool menu_redraw_required = 0;
-int encoderValue = 0;  // Count of encoder turns and cursor position
+int encoderValue = 0;  // Count of encoder turns = cursor position
 bool lastStateA = HIGH; // Previous state of encoder pin A
-bool currentMenu = 0; // 0: spice menu,   1: amount menu
 uint8_t amountColumn = 0; // 0: spice integer,   1: spice fraction,   2: spice unit,   3: confirm
 uint8_t maxBound = 1; // max index for a menu
-// Buffer variable for spice amount menu:
+uint8_t spiceListMax = 8; // maximum list capacity, to determine screen update
+uint8_t summaryListMax = 5;
+uint8_t currentMenu = 0; // 0: setup menu,   1: spice menu,   2: amount menu,   3: summary menu
+unsigned long lastSummaryTime = 0; // time (millis) of last summary menu. For auto timeout.
+unsigned long currentTime;
+unsigned long interval = 15000; // 15 second interval before timeout
+int pushState; // current button reading
+int stopState;
+int pushCount = 0;
+int stopCount = 0;
+
+// Buffer variables for spice amount menu:
 int tempSpiceNumber = 0; 
 int tempSpiceInt = 0;
 float tempSpiceFraction = 0.0;
-uint8_t tempSpiceDivider = 1; // unit conversion
+uint8_t tempSpiceDivider = 6; // unit conversion
 String tempSpiceString = "";
 
 const char *menu_strings[totalSpices+1] = { "Confirm order", "Spice 1", "Spice 2", "Spice 3", "Spice 4", "Spice 5", "Spice 6", "Spice 7", "Spice 8", "Spice 9", "Spice 10"};
+
+const char *spicySaying[5] = {"Have a spicy day!",
+        "You're on fire!",
+        "Spice up your life!",
+        "Blend it like it's hot!",
+        "Keep it spicy!"};
+long randomNumber; // for random spicy saying
 
 
 void setup() {
@@ -104,6 +120,8 @@ void setup() {
 
   u8g.setColorIndex(1);      // Set color to white  
   menu_redraw_required = 1;     // force initial redraw
+
+  randomSeed(analogRead(A5)); // Note: ensure nothing is connected to A5 (in Aux-2 pins on Ramps)
 }
 
 void loop() {
@@ -125,44 +143,42 @@ void loop() {
     }
   }
 
-  //Check for user click input
-  if(!digitalRead(LCD_push)){
-    while(!digitalRead(LCD_push)){
-      delay(100); // add delay to prevent double counting button press
-    }
-    isBLEOrder = 0;
-  }
-
-  // Setup picture loop for BLE order
-  if(isBLEOrder){
+  // Update current LCD menu
+  if(menu_redraw_required){
     u8g.firstPage();
-    do {
-      drawSetup();
-    } while( u8g.nextPage());
+    do{
+      if(currentMenu == 0){
+        // setup menu
+        drawSetup();
+      }else if(currentMenu == 1){
+        // spice selection menu
+        drawSpiceMenu();
+      }else if(currentMenu == 2){
+        // amount menu
+        drawAmountMenu();
+      }else{
+        // summary menu
+        drawSummary();
+      }
+    } while(u8g.nextPage());
+
+    menu_redraw_required = 0;
   }
 
-  //Spice menu loop for manual order
-  while(!isOrderInput && !isBLEOrder){
+  // Check for user inputs
+  updateLCD();
 
-    // Check if data is available from LCD
-    updateLCD();
-  
-    // update scroll bar
-    if (menu_redraw_required) {
-      u8g.firstPage();
-      do  {
-        // check for current menu to draw
-        if(!currentMenu){
-          drawSpiceMenu();
-        }else if(currentMenu){
-          drawAmountMenu();
-        }
-          
-      } while( u8g.nextPage() );
-      menu_redraw_required = 0;
+  // Reset to setup menu if summary has been up
+  if(lastSummaryTime != 0){
+    currentTime = millis();
+
+    // If 15 seconds have passed:
+    if(currentTime - lastSummaryTime >= interval){
+      currentMenu = 0;
+      menu_redraw_required = 1;
+      lastSummaryTime = 0;
     }
   }
-  
 
   // Process spice data if the order has been received and not mixed
   if (!isOrderMixed) {
@@ -211,13 +227,14 @@ void sendOrderMixedStatus() {
   Serial1.println("ORDER_MIXED:1");
 
   // quick beep for user
-  analogWrite(LCD_beeper, 512);
+  analogWrite(LCD_beeper, 256); // comment out for quiet mode
   delay(1000);
   analogWrite(LCD_beeper, 0);
 
-  // Finished job picture loop
-  drawSummary();
-  
+  // Set current menu to summary
+  currentMenu = 3;  
+  menu_redraw_required = 1;
+  randomNumber = random(0, 5); // random spicy saying for the summary screen
 }
 
 void processReceivedIngredients(String data) {
@@ -455,7 +472,7 @@ void calibrate() {
     u8g.drawStr(0, 0, "Calibrating...");
   } while( u8g.nextPage());  
 
-  delay(2000); // Remove this line!!!
+  delay(2000); // Note: Remove this line!!!
 }
 
 
@@ -488,21 +505,21 @@ void drawSetup() {
 
 void updateLCD() {
   // Read the current state of the encoder pins
-  bool currentStateA = digitalRead(LCD_DT);
-  bool currentStateB = digitalRead(LCD_CLK);
+  int currentStateA = digitalRead(LCD_DT);
+  int currentStateB = digitalRead(LCD_CLK);
 
   // Determine if new input is received (LOW)
   if (lastStateA == HIGH && currentStateA == LOW) {
     // change cursor position based on direction of B
     encoderValue += (currentStateB == HIGH) ? 1 : -1;
-    delay(25); // slight delay for smoothness
+    //delay(100); // slight delay for smoothness. Consider changing this to debounce delay
     menu_redraw_required = 1;
 
     // Keep cursor in menu bounds
-    if(!currentMenu){
+    if(currentMenu==1){
       // Spice menu bounds
       maxBound = totalSpices; // 10 spices plus confirm button
-    }else{
+    }else if(currentMenu==2){
       // Amount menu bounds
       if(amountColumn == 0){
         // Spice amount (integer)
@@ -516,40 +533,49 @@ void updateLCD() {
         // Spice unit
         maxBound = 2; // tsp, Tbsp, oz
       }
+    }else if(currentMenu==3){
+      // Summary menu bounds
+      maxBound = numSpicesOrdered-1;
     }
 
     if(encoderValue > maxBound){
         encoderValue = 0;
         delay(50); // slight delay to avoid jitter 
-      }
-      else if(encoderValue < 0){
-        encoderValue = maxBound-1;
+      }else if(encoderValue < 0){
+        encoderValue = maxBound;
         delay(50); // slight delay to avoid jitter 
       }
-
   }
 
-  // Check for encoder button push
-  if(!digitalRead(LCD_push)){
-    while(!digitalRead(LCD_push)){
-      delay(100); // add delay to prevent double counting button press
-    }
+  // Update last state
+  lastStateA = currentStateA;
+
+  // Read encoder button
+  pushState = digitalRead(LCD_push);
+  // If button is not pressed, reset counter
+  if(pushState == HIGH){
+    pushCount = 0;
+  }
+
+  // if button pressed and not previously counted
+  if(pushState == LOW && pushCount == 0){
     menu_redraw_required = 1;
     
     // Change menu or column depending on curent screen
-    if(!currentMenu){
+    if(currentMenu == 1){
       // Spice menu:
       if(encoderValue == 0){
         processReceivedIngredients(tempSpiceString); // process data into spiceArray
-        isOrderInput = 1; // confirm order
         tempSpiceString = ""; // clear buffer
+        pushCount = 1; // prevent button bouncing
       }else{
       tempSpiceNumber = encoderValue; // save selected spice into buffer
-      currentMenu = !currentMenu; // advance from spice menu to amount menu
+      currentMenu++; // advance from spice menu to amount menu
       encoderValue = 0; // reset cursor value
+      pushCount = 1; // prevent button bouncing
     }
     // Amount menu:
-    }else if(amountColumn != 3){
+    }else if(currentMenu == 2 && amountColumn != 3){
       // save selected amount into buffer
       if(amountColumn == 0){
         tempSpiceInt = encoderValue; // whole number
@@ -566,58 +592,83 @@ void updateLCD() {
       }
       amountColumn++; // advance column number in amount menu
       encoderValue = 0; // reset cursor
-    }else{
+      pushCount = 1; // prevent button bouncing
+    }else if(currentMenu == 2 && amountColumn == 3){
       tempSpiceString += String(tempSpiceNumber) + ":"; // store spice number and amount 
       tempSpiceString += String((tempSpiceInt + tempSpiceFraction) / tempSpiceDivider) + ";";
       tempSpiceNumber = 0; // clear buffer
       tempSpiceInt = 0;
       tempSpiceFraction = 0.0;
-      tempSpiceDivider = 1;
+      tempSpiceDivider = 6;
       amountColumn = 0; // reset column
-      currentMenu = !currentMenu; // if amount confirmed, return to spice menu
+      currentMenu = 1; // if amount confirmed, return to spice menu
+      pushCount = 1; // prevent button bouncing
+    }else if(currentMenu == 3){
+      // Summary screen
+      currentMenu = 0; // return to setup menu
+      pushCount = 1; // prevent button bouncing
+    }else{
+      // Setup screen
+      currentMenu = 1; // advance to spice menu
+      pushCount = 1; // prevent button bouncing
     }
-    
   }
 
-  // Check for stop button push
-  if(!digitalRead(LCD_stop)){
-    while(!digitalRead(LCD_stop)){
-      delay(100); // add delay to prevent double counting button press
-    }
+  // Read stop button
+  stopState = digitalRead(LCD_stop);
+  // If button is not pressed, reset counter
+  if(stopState == HIGH){
+    stopCount = 0;
+  }
+
+  // if button pressed and not previously counted
+  if(stopState == LOW && stopCount == 0){
     menu_redraw_required = 1;
 
     // Change menu or column depending on curent screen
-    if(!currentMenu){
+    if(currentMenu == 1){
       // Spice menu:
-      isBLEOrder = 1; // return to start menu
+      currentMenu--; // return to setup menu
+      tempSpiceString = ""; // wipe data buffer
       encoderValue = 0; // reset cursor
-    }
-    // Amount menu:
-    else if(amountColumn != 0){
+      stopCount = 1;
+    }else if(currentMenu == 2 && amountColumn != 0){
+      // Amount menu:
       amountColumn--; // retract column number in amount menu
       encoderValue = 0; // reset cursor
-      // delete spice amount from buffer
-    }else{
-      currentMenu = !currentMenu; // return to spice menu
+      stopCount = 1;
+    }else if(currentMenu == 2 && amountColumn == 0){
+      currentMenu--; // return to spice menu
       encoderValue = 0; // reset cursor
+      stopCount = 1;
+    }else if(currentMenu == 3){
+      // Summary screen
+      currentMenu = 0; // reset to setup screen
+      stopCount = 1;
     }
-  }
-
-  // Update last state
-  lastStateA = currentStateA;
+  }  
 }
 
 void drawSummary() {
   u8g.firstPage();
   do {
-    u8g.drawStr(0, 0, "Complete! Stay Spicy!");
-    int h = u8g.getFontAscent() - u8g.getFontDescent();
-    u8g.drawStr(0, h+1, "Spice");
-    u8g.drawStr(64, h+1, "Amount (oz)");
-    u8g.drawHLine(0, 2*h+2, 128);
+    uint8_t j, h;
+    u8g_uint_t w, d;
+    
+    w = u8g.getWidth();
+    d = (w-u8g.getStrWidth("Blending complete!"))/2;
+    u8g.drawStr(d, 0, "Blending complete!");
+    h = u8g.getFontAscent() - u8g.getFontDescent();
 
-    for(int j = 0; j < numSpicesOrdered; j++) {
-      u8g.drawStr(0, (j+2)*h+3, spiceArray[j][0].c_str()); // convert String object to const char*
+    d = (w-u8g.getStrWidth(spicySaying[randomNumber]))/2;
+    u8g.drawStr(d, h+1, spicySaying[randomNumber]);
+
+    u8g.drawStr(0, 2*h+1, "Spice");
+    u8g.drawStr(64, 2*h+1, "Amount (oz)");
+    u8g.drawHLine(0, 3*h+2, 128);
+
+    for(j = 0; j < numSpicesOrdered; j++) {
+      u8g.drawStr(0, (j+3)*h+3, spiceArray[j][0].c_str()); // convert String object to const char*
 
       // Find decimal point index
       int decimalIndex = spiceArray[j][1].indexOf('.');
@@ -626,12 +677,14 @@ void drawSummary() {
       // convert String object to const char*
       const char *truncatedAmount = truncatedString.c_str();
 
-      u8g.drawStr(64, (j+2)*h+3, truncatedAmount);
+      u8g.drawStr(64, (j+3)*h+3, truncatedAmount);
     }
 
     // eventually add a cursor to fit all spices on screen and allow user navigation
 
   } while( u8g.nextPage());
+
+  lastSummaryTime = millis();
 }
 
 void drawSpiceMenu() {
@@ -714,26 +767,49 @@ void drawAmountMenu(){
     // check for cursor box
     if (i == amountColumn) {
       if(i != 3){
-        u8g.drawBox(m-5, 3*h+2, 11, 18);
-        u8g.setDefaultBackgroundColor();
-        u8g.drawTriangle(m-4,4*h, m+4,4*h, m,3*h+2); // top triangle
-        u8g.drawTriangle(m-4,5*h+1, m,6*h-2, m+4,5*h+1); // bottom triangle
         // Selection values
         if(i==0){
-          itoa(tempSpiceInt, buffer, 10);
+          u8g.drawBox(m-5, 3*h+2, 11, 18); //cursor box
+          u8g.setDefaultBackgroundColor();
+          u8g.drawTriangle(m-4,4*h, m+4,4*h, m,3*h+2); // top triangle
+          u8g.drawTriangle(m-4,5*h+1, m,6*h-2, m+4,5*h+1); // bottom triangle
+          itoa(encoderValue, buffer, 10);
           u8g.drawStr(m-2, 4*h, buffer); // whole number
         }else if(i==1){
           u8g.setColorIndex(1);
-          u8g.drawBox(m-13, 3*h+2, 27, 18); //expand the box
+          u8g.drawBox(m-13, 3*h+2, 27, 18); // larger box
           u8g.setDefaultBackgroundColor();
-          dtostrf(tempSpiceFraction, 5, 3, buffer);
-          u8g.drawStr(m-12, 4*h, buffer); // fraction
+          u8g.drawTriangle(m-4,4*h, m+4,4*h, m,3*h+2); // top triangle
+          u8g.drawTriangle(m-4,5*h+1, m,6*h-2, m+4,5*h+1); // bottom triangle
+          if(encoderValue == 1){
+            u8g.drawStr(m-7, 4*h, "1/8"); // fraction
+          }else if(encoderValue == 2){
+            u8g.drawStr(m-7, 4*h, "1/4"); 
+          }else if(encoderValue == 3){
+            u8g.drawStr(m-7, 4*h, "3/8"); 
+          }else if(encoderValue == 4){
+            u8g.drawStr(m-7, 4*h, "1/2"); 
+          }else if(encoderValue == 5){
+            u8g.drawStr(m-7, 4*h, "5/8"); 
+          }else if(encoderValue == 6){
+            u8g.drawStr(m-7, 4*h, "3/4"); 
+          }else if(encoderValue == 7){
+            u8g.drawStr(m-7, 4*h, "7/8"); 
+          }else{
+            u8g.drawStr(m-2, 4*h, "0");
+          }
+          
         }else if(i==2){
-          if(tempSpiceDivider==1){
+          u8g.drawBox(m-11, 3*h+2, 23, 18); //medium sized cursor box
+          u8g.setDefaultBackgroundColor();
+          u8g.drawTriangle(m-4,4*h, m+4,4*h, m,3*h+2); // top triangle
+          u8g.drawTriangle(m-4,5*h+1, m,6*h-2, m+4,5*h+1); // bottom triangle
+
+          if(encoderValue==2){
             u8g.drawStr(m-u8g.getStrWidth("oz")/2, 4*h, "oz"); // unit: oz
-          }else if(tempSpiceDivider==2){
+          }else if(encoderValue==1){
             u8g.drawStr(m-u8g.getStrWidth("Tbsp")/2, 4*h, "Tbsp"); // unit: Tbsp
-          }else if(tempSpiceDivider==6){
+          }else if(encoderValue==0){
             u8g.drawStr(m-u8g.getStrWidth("tsp")/2, 4*h, "tsp"); // unit: tsp
           }
         }
@@ -757,8 +833,23 @@ void drawAmountMenu(){
         itoa(tempSpiceInt, buffer, 10);
         u8g.drawStr(m-2, 4*h, buffer); // whole number
       }else if(i==1){
-        dtostrf(tempSpiceFraction, 5, 3, buffer);
-        u8g.drawStr(m-12, 4*h, buffer); // fraction
+        if(tempSpiceFraction == 0.125){
+            u8g.drawStr(m-7, 4*h, "1/8"); // fraction
+          }else if(tempSpiceFraction == 0.25){
+            u8g.drawStr(m-7, 4*h, "1/4"); 
+          }else if(tempSpiceFraction == 0.375){
+            u8g.drawStr(m-7, 4*h, "3/8"); 
+          }else if(tempSpiceFraction == 0.5){
+            u8g.drawStr(m-7, 4*h, "1/2"); 
+          }else if(tempSpiceFraction == 0.625){
+            u8g.drawStr(m-7, 4*h, "5/8"); 
+          }else if(tempSpiceFraction == 0.75){
+            u8g.drawStr(m-7, 4*h, "3/4"); 
+          }else if(tempSpiceFraction == 0.875){
+            u8g.drawStr(m-7, 4*h, "7/8"); 
+          }else{
+            u8g.drawStr(m-2, 4*h, "0");
+          }
       }else if(i==2){
         if(tempSpiceDivider==1){
           u8g.drawStr(m-u8g.getStrWidth("oz")/2, 4*h, "oz"); // unit: oz
